459a839148ea347f3e5839e889258ec3cfd8fd78
Ranjith kumar <ranjit4072@gmail.com>
2021-03-03 19:31:48 +0530

    Added mock for GET api with param and POST api call.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { getDummyData } from "./Service/Api";
+import { addTodo, getDummyData, getDummyDataByID } from "./Service/Api";
 
 const App = () => {
   const [todo, setTodo] = useState([]);
@@ -19,12 +19,24 @@ const App = () => {
   if (todo.length === 0) {
     return <div> No Todo Found.</div>;
   }
+
+  const addTodoItem = async () => {
+    let obj = {
+      title: "foo",
+      body: "bar",
+      userId: 1
+    };
+    let data = await addTodo(obj);
+    console.log(data);
+  };
+
   return (
     <div className="App">
       <h1>Axios Mock Demo</h1>
       {todo.map((item, index) => {
         return <div key={item.id}>{item.title}</div>;
       })}
+      <button onClick={() => addTodoItem()}> Add todo </button>
     </div>
   );
 };
--- src/Service/Api.js
+++ src/Service/Api.js
@@ -4,19 +4,49 @@ import MockAdapter from "axios-mock-adapter";
 let mock = new MockAdapter(axios);
 
 // Mock GET api
-// mock.onGet("https://jsonplaceholder.typicode.com/todos/").reply(200, [
-//   { id: 1, title: "To code" },
-//   { id: 2, title: "To sleep" },
-//   { id: 3, title: "To eat" }
-// ]);
+mock.onGet("https://jsonplaceholder.typicode.com/todos/").reply(200, [
+  { id: 1, title: "To code" },
+  { id: 2, title: "To sleep" },
+  { id: 3, title: "To eat" }
+]);
 
 // Mock GET api - error
-mock.onGet("https://jsonplaceholder.typicode.com/todos/").networkError();
+// mock.onGet("https://jsonplaceholder.typicode.com/todos/").networkError();
+
+//Mock GET api with params
+// mock
+//   .onGet("https://jsonplaceholder.typicode.com/todos/1")
+//   .reply(200, [{ id: 1, title: "To code" }]);
+
+//Mock POST api
+mock
+  .onPost("https://jsonplaceholder.typicode.com/posts")
+  .reply(201, { message: "success" });
 
 export const getDummyData = async () => {
   let url = "https://jsonplaceholder.typicode.com/todos/";
   let data = await axios(url, {
     method: "GET"
+  });
+  return data;
+};
+
+export const getDummyDataByID = async () => {
+  let url = "https://jsonplaceholder.typicode.com/todos/1";
+  let data = await axios(url, {
+    method: "GET"
+  });
+  return data;
+};
+
+export const addTodo = async (obj) => {
+  let data = await axios({
+    url: "https://jsonplaceholder.typicode.com/posts",
+    method: "POST",
+    data: obj,
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
   });
   return data;
 };
